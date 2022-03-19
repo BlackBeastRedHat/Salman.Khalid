d1bdc56db78e25ecc9e0609305800134fea23d65
BlackBeastRedHat <101577003+BlackBeastRedHat@users.noreply.github.com>
2022-03-19 04:11:56 -0700

    BlackHatBeast
    pkg install update
    pkg install upgrade
    pkg install git
    pkg install python
    pkg install python2
    pip2 install requests
    pip2 install mechanize
    git clone https://github.com/RedHatBeast/salmankhalid.git
    cd salmankhalid
    python2 salman.py

---
+++ BlackHatBeast.py
@@ -0,0 +1,36 @@
+import time
+a = time.ctime()
+print a 
+
+
+<a href="#"><img title="Made in Pakistan" src="https://img.shields.io/badge/MADE%20IN-Pakistan-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<a href="#"><img title="Salman Crack" src="https://raw.githubusercontent.com/htr-tech/release-download/master/images/banner/pakcrak.png"></a>
+<a href="#"><img title="Salman Crack" src=".__src__/salmancrak.png"></a>
+<p align="center">
+<a href="https://github.com/RedHatBeast"><img title="Author" src="https://img.shields.io/badge/Author-htr--tech-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+@@ -22,20 +22,21 @@
+
+#### > Run : `python2 Salman.py`
+
+#### USERNAME & PASSWORD : [Send Message Here](https://fb.com/salman.khalid.376043)
+#### USERNAME : `Salman`
+#### PASSWORD : `Khalid`
+
+## Single Command :
+```
+apt update ; apt install git python python2 -y ; git clone https://github.com/RedHatBeast/salmankhalid.git ; cd salmankhalid ; python2 crack.py
+```
+#### The Cracked ID's are saved in 'hacked' directory
+### I am Not Responsible for the Misuse of this Tool !!
+
+
+### << I AM NOT RESPONSIBLE FOR MISUSE >>
+### <<< If you copy , Then Give me The Credits >>>
+
+## Features :
+@@ -44,9 +45,9 @@ apt update ; apt install git python python2 -y ; git clone https://github.com/RedHatBeast/salmankhalid.git
+#### [+] All Pakistani Operators are Available !
+#### [+] Easy for Beginners !
